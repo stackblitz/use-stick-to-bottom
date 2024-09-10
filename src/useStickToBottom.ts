e09f5ba2d323c7f8f2b23ff3f818fa1d60ae79b1
Sam Denty <sam@samdenty.com>
2024-09-10 18:45:55 +0100

    fix

--- src/useStickToBottom.ts
+++ src/useStickToBottom.ts
@@ -201,7 +201,7 @@ export const useStickToBottom = (options: StickToBottomOptions = {}) => {
      */
     if (deltaY < 0) {
       setEscapedFromLock(true);
-      updateIsAtBottom(false);
+      updateIsAtBottom();
     }
   }, []);
 
