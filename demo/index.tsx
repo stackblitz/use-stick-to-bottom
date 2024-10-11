913b02cdc4cac0145fdc6ca7e6ec348ca1e75a39
Sam Denty <sam@samdenty.com>
2024-10-11 09:30:47 +0100

    initial commit

---
+++ demo/index.tsx
@@ -0,0 +1,7 @@
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Demo } from './Demo.js';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<Demo />);
